3da84940e9149926baa8afbcb8a2de0ec9dcf23e
Daria Diakonenko <daria_diakonenko@calmerry.com>
2022-04-28 16:46:01 +0300

    init commit

---
+++ lab6/src/index.js
@@ -0,0 +1,18 @@
+const { decode, getPublicKey } = require('./secure/secure');
+const { initDisk } = require('./init/init');
+const { cliLine } = require('./cli/cli');
+const { verifyUserWithRules } = require('./secure/access');
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
+(async () => {
+    try {
+        initDisk();
+        const disk = decode();
+        const publicKey = getPublicKey(disk);
+        const userData = await verifyUserWithRules(disk);
+        await cliLine({ user: userData.login, currentDir: '/', disk, publicKey });
+    } catch (e) {
+        console.log('\x1b[31m%s\x1b[0m', e.message);
+    }
+
+})();
